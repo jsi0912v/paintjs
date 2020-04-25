a05a3702637b77554db73bdb03c684adf94ed749
jsi0912v <jsi0912v@gmail.com>
2020-04-25 09:06:51 +0900

    canvas painting color change

---
+++ app.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById("jsCanvas");
+const colors = document.getElementsByClassName("jsColor");
+console.log(canvas);
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2;
+
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if (!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        //ctx.closePath();
+    }
+
+    //console.log(X, Y);
+}
+
+function handleColorCilck(event){
+    
+    const color = event.target.style.backgroundColor;
+    console.log(color);
+
+    ctx.strokeStyle = color;
+
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach( color => color.addEventListener("click", handleColorCilck));
